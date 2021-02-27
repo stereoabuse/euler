3a9747961021c904c111cfea929ca663e4352253
Chris Bowman <stereoabusestudios@gmail.com>
2021-02-26 22:48:35 -0500

    faster

--- problems/prob_005.py
+++ problems/prob_005.py
@@ -3,13 +3,13 @@
 #                     https://projecteuler.net/problem=005                     #
 #                                                                              #
 # Author:   Chris B.                                                           #
-# Date:     November 05, 2020                                                  #
+# Date:     February 26, 2020                                                  #
 ################################################################################
 
 def prob_005(n=20):
     "Returns smallest number that is evenly divisible by all the numbers from 1 to n"
     ans = 1
-    for i in range(1, 21):
+    for i in range(1, n + 1):
         ans *= floor(i / gcd(i, ans))
     return ans
 
